a733e4362a1b53e9e1b7eb1a0e6c6b06b478e0b5
shivam333maurya <shivam333maurya>
2022-04-19 18:25:09 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Hello from "./Containers/index"
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+<Hello/>       
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hello = () => {
+  return (
+    <>
+      <div>hI</div>
+      <div>Hello</div>
+      <div>wellcome</div>
+    </>
+  );
+};
+
+export default Hello;
